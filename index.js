29c4d4e6b9c467d47ecff3f8396fb38fd4b68467
Andrew Thomas <athomas94@gmail.com>
2023-11-16 15:08:47 -0500

    Adding the updated JS file with all functionality in place

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage (sourcePath, left, bottom) {
+    let placeholder = document.createElement('img')
+    placeholder.src = sourcePath
+    placeholder.style.position = 'fixed'
+    placeholder.style.left = left
+    placeholder.style.bottom = bottom
+    document.body.append(placeholder)
+    return placeholder
+}
+
+function newItem (sourcePath, left, bottom) {
+    let placeholder2 = newImage (sourcePath, left, bottom)
+
+    placeholder2.addEventListener('dblclick', function(){
+        placeholder2.remove()
+    })
+}
+
+newImage ('assets/green-character.gif', '100px', '100px')
+newImage ('assets/pine-tree.png', '450px', '200px')
+newImage ('assets/tree.png', '200px', '300px')
+newImage ('assets/pillar.png', '350px', '100px')
+newImage ('assets/crate.png', '150px', '200px')
+newImage ('assets/well.png', '500px', '425px')
+
+newItem ('assets/sword.png', '500px', '405px')
+newItem ('assets/shield.png', '165px', '185px')
+newItem ('assets/staff.png', '600px', '100px')
